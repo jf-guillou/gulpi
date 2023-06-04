5694cd4f2edd3a52de6a744780235f919e16d632
Jeff Guillou <jean.f.guillou@gmail.com>
2023-06-04 21:47:30 +0200

    Added some API methods

--- app/src/main/java/fr/klso/gulpi/services/GlpiApi.kt
+++ app/src/main/java/fr/klso/gulpi/services/GlpiApi.kt
@@ -1,13 +1,43 @@
 package fr.klso.gulpi.services
 
 import fr.klso.gulpi.models.ApiSession
+import fr.klso.gulpi.models.Item
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface GlpiApi {
     @GET("apirest.php/initSession")
     suspend fun initSession(
-        @Header("Authorization") authorization: String,
-        @Header("App-Token") appToken: String
+        @Header("App-Token") appToken: String,
+        @Header("Authorization") authorization: String
     ): ApiSession
+
+    @GET("apirest.php/{itemType}/{itemId}")
+    suspend fun getItem(
+        @Header("App-Token") appToken: String,
+        @Header("Session-Token") sessionToken: String,
+        @Path("itemType") itemType: String,
+        @Path("itemId") itemId: String
+    ): Item
+
+    @GET("apirest.php/{itemType}/")
+    suspend fun getItems(
+        @Header("App-Token") appToken: String,
+        @Header("Session-Token") sessionToken: String,
+        @Path("itemType") itemType: String
+    ): Unit
+
+    @GET("apirest.php/search/{itemType}/")
+    suspend fun search(
+        @Header("App-Token") appToken: String,
+        @Header("Session-Token") sessionToken: String,
+        @Path("itemType") itemType: String
+    ): Unit
+
+    @GET("apirest.php/getGlpiConfig")
+    suspend fun config(
+        @Header("App-Token") appToken: String,
+        @Header("Session-Token") sessionToken: String,
+    ): Unit
 }
