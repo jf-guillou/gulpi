2dbe55b289456aa798bd2362777a45de8d3ce36a
Jeff Guillou <jean.f.guillou@gmail.com>
2023-06-27 22:12:28 +0200

    Added raw HomeScreen buttons

--- app/src/main/java/fr/klso/gulpi/screens/HomeScreen.kt
+++ app/src/main/java/fr/klso/gulpi/screens/HomeScreen.kt
@@ -1,10 +1,58 @@
 package fr.klso.gulpi.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import fr.klso.gulpi.navigation.Scan
+import fr.klso.gulpi.navigation.SearchForm
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Text(text = "HomeScreen")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 20.dp)
+    ) {
+        Button(onClick = {
+            navController.navigate(Scan.route) {
+                launchSingleTop = true
+            }
+        }) {
+            Text("Scan")
+        }
+        Button(onClick = {
+            navController.navigate(SearchForm.route) {
+                launchSingleTop = true
+            }
+        }) {
+            Text("Search")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 20.dp)
+    ) {
+        Button(onClick = {}) {
+            Text("Scan")
+        }
+        Button(onClick = { }) {
+            Text("Search")
+        }
+    }
 }
