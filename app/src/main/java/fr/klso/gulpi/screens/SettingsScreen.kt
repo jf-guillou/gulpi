7426ea39d5e521b1e3c9343ce53679d4dce8c16a
Jeff Guillou <jean.f.guillou@gmail.com>
2023-08-21 21:45:48 +0200

    Nicer login & credentials UI mockup

--- app/src/main/java/fr/klso/gulpi/screens/SettingsScreen.kt
+++ app/src/main/java/fr/klso/gulpi/screens/SettingsScreen.kt
@@ -3,15 +3,9 @@ package fr.klso.gulpi.screens
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.alorma.compose.settings.ui.SettingsGroup
-import fr.klso.gulpi.screens.settings.SettingsGlpiAppToken
-import fr.klso.gulpi.screens.settings.SettingsGlpiUrl
-import fr.klso.gulpi.screens.settings.SettingsGlpiUserToken
 
 @Composable
 fun SettingsScreen() {
     SettingsGroup(title = { Text("Server") }) {
-        SettingsGlpiUrl()
-        SettingsGlpiAppToken()
-        SettingsGlpiUserToken()
     }
 }
